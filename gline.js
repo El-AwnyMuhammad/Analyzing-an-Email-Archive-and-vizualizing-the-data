0568fcccd98dd7eb7fbd8699b7e147dd7f938e5b
El-Awny Muhammad <elawnymohamad@gmail.com>
2021-01-31 02:02:07 +0200

    First commit

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','ufp.pt','ucdavis.edu','berkeley.edu','columbia.edu','uct.ac.za','mac.com','gmail.com','unl.edu'],
+['2005-12',57,12,10,11,12,13,14,12,10,12],
+['2006-01',45,17,19,15,12,9,8,9,7,3]
+];
